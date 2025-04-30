9373d4fea8142731b3b3e9493e7feef5602b4f4b
Syd-xo <sydneyaisha4@gmail.com>
2025-04-30 13:04:34 +0300

    Done with assignment 1,2,3

---
+++ assignment2/ShoppingCart.py
@@ -0,0 +1,65 @@
+class ShoppingCart:
+
+    def __init__(self):
+       self.items = []
+
+    def add_item(self, item_name: str, qty: int, unit_price: float):
+       self.item_name = item_name
+       self.qty = qty
+       self.unit_price = unit_price
+
+       self.items.append((item_name, qty, unit_price)) #This creates a tuple
+
+    def remove_item(self, item_name: str):
+       for item in self.items:
+           if item[0] == item_name:
+               self.items.remove(item)
+
+    def calculate_total(self) -> float:
+
+       total = 0
+
+       for name, qty, price in self.items:
+           total += qty * price
+
+       return total
+
+    def summary(self):
+
+       print("Cart content")
+       for name, qty, price in self.items:
+           print(f"{name}: {qty} @ Ksh {price:.3f}") #This is a formatted string ie, you can change the format of a string
+       print(f"Total: Ksh {self.calulate_total:():.3f}")
+
+
+def add_item(name, quantity, price):
+    pass
+
+
+if __name__ == "__main__":
+   cart: add_item("kiwi",69, 70.8)
+   cart: add_item("apple", 45, 31.4)
+   cart: add_item("grape", 60, 89.2)
+
+   print(">>> Ordinary Cart<<<")
+   cart.summary()
+
+
+
+
+
+
+
+
+
+# def numbers(*num: int): #putting * before num makes it possible to put multiple items in a list, without errors
+#     return int
+# print(numbers(1,2,3,4,5))
+#
+#
+#
+# obj: ShoppingCart = ShoppingCart()
+# obj2:ShoppingCart = ()
+#
+# print(obj.area(7))
+# print(obj2.area(7.7))
---
+++ assignment3/DateCalculator.py
@@ -0,0 +1,36 @@
+class DateCalculator:
+    def __init__(self, year: object, month: object, day: object) -> None:
+        self.year= year
+        self.month = month
+        self.day = day
+
+    def calculate_day_of_the_week(self):
+        #Adjust month and year for January and February
+        if self.month == 1 or self.month == 2:
+            self.month += 12
+            self.year -= 1
+
+        q = self.day
+        m = self.month
+        K = self.year % 100
+        J = self.year // 100
+
+        # Apply Zeller's Congruence formula
+        h = (q+ (13* (m+1)) //5+K+(K//4) +(J//4) +5*J) % 7
+
+        # Mapping h to days of the week
+        days = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+        return days[h]
+
+#Taking user input
+year = int(input("Enter the year (e.g., 2024): "))
+month = int(input("Enter the month (e.g., 1-12): "))
+day = int(input("Enter the day (e.g., 1-31): "))
+
+date =  DateCalculator(year, month,day)
+day_of_week = date.calculate_day_of_the_week()
+
+months = ["", "January","February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+#output
+print(f"\nYay! {months [month]} {day}, {year} was a {day_of_week}! 🌸✨")
